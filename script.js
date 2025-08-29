d08aa974c5f3c8cc4a4cf3dde981279e65b08ab0
Anonim Person <akbarhansem01@gmail.com>
2025-08-29 13:21:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-// Toggle navbar mobile
+// Navbar mobile toggle
 const menuToggle = document.getElementById("menu-toggle");
 const navbar = document.getElementById("navbar");
 
 menuToggle.addEventListener("click", () => {
-  if (navbar.style.display === "block") {
-    navbar.style.display = "none";
-  } else {
-    navbar.style.display = "block";
-  }
+  navbar.style.display = navbar.style.display === "block" ? "none" : "block";
 });
+
+// Buka Google Maps dari destinasi
+function openMap(url) {
+  window.open(url, "_blank");
+}
