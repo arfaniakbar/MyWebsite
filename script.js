ed1839664cd4ef0c180494c13aa90d739b62e18d
Anonim Person <akbarhansem01@gmail.com>
2025-08-29 14:34:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,15 +3,14 @@ const menuToggle = document.getElementById("menu-toggle");
 const navbar = document.getElementById("navbar");
 
 menuToggle.addEventListener("click", () => {
-  // Jika menu terlihat → sembunyikan, kalau tersembunyi → tampilkan
-  if (navbar.style.display === "block") {
+  if (navbar.style.display === "flex") {
     navbar.style.display = "none";
   } else {
-    navbar.style.display = "block";
+    navbar.style.display = "flex";
   }
 });
 
-// === Buka Google Maps dari Destinasi ===
+// === Example Function for Destinasi ===
 function openMap(url) {
-  window.open(url, "_blank"); // buka link di tab baru
+  window.open(url, "_blank");
 }
